4b5b5e6be91fa01c7a6d872287d33ed6f439baf5
xung <xungnguyen558@gmail.com>
2018-04-21 11:23:54 +0700

    commit dau tien

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password ="";
+$db = 'basiccrud';
+
+$con = mysqli_connect($servername, $username, $password,$db);
+
+if(!$con){
+  die("Connection failed: ".mysqli_connect_error());
+}
+ ?>
