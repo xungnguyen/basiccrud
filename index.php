708c64a9377acac49f60770e796fce63678b395f
xung <xungnguyen558@gmail.com>
2018-04-21 11:32:51 +0700

    sql statement, query, result

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 include('db.php');
 
-print_r($con);
+//print_r($con);
  ?>
 
 <h1>Homepage</h1>
+
+<table border="1">
+  <tr>
+    <th>title</th>
+    <th>content</th>
+  </tr>
+
+
+<?php
+  $sql="SELECT * FROM `content`"; //sql statement
+  $query = mysqli_query($con, $sql); //sql query
+  $result = mysqli_fetch_assoc($query); //sql result
+
+  print_r($result);
+?>
+  <tr>
+    <td>testing</td>
+    <td>testing</td>
+  </tr>
+
+
+</table>
