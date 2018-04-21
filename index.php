cf29af3de5345acd640b0ef898e2a125df3ae53c
xung <xungnguyen558@gmail.com>
2018-04-21 11:46:43 +0700

    for and foreach loop

--- index.php
+++ index.php
@@ -18,12 +18,40 @@ include('db.php');
   $query = mysqli_query($con, $sql); //sql query
   $result = mysqli_fetch_assoc($query); //sql result
 
-  print_r($result);
+  //print_r($result);
+
+  //foreach(){
 ?>
   <tr>
     <td>testing</td>
     <td>testing</td>
   </tr>
-
-
+<?php// } ?>
 </table>
+<br/><br/><br/><br/><br/><br/><br/>
+
+<?php
+          //0    //1   //2    //3     //4    //5
+$arr = ['moi', 'hai', 'ba', 'four', 'five', 'ennum'];
+echo json_encode($arr);
+//echo $arr[2];
+
+//echo sizeof($arr);
+
+echo "for loop";
+echo "<br/>";
+for( $i=0;$i<sizeof($arr);$i++ ){
+  echo $arr[$i];
+  echo "<br/>";
+}
+
+echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+//echo "foreach loop";
+$arr = (object) $arr;
+echo json_encode($arr);
+foreach( $arr as $X ){
+  echo "<h2>".$X."</h2>";
+}
+
+
+ ?>
