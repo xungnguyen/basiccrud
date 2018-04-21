136ad1dcaf17c16cf5b25d8a1c99a6d3e0cfac07
xung <xungnguyen558@gmail.com>
2018-04-21 11:54:44 +0700

    read from db

--- index.php
+++ index.php
@@ -14,44 +14,42 @@ include('db.php');
 
 
 <?php
-  $sql="SELECT * FROM `content`"; //sql statement
+  $sql= "SELECT * FROM `content`"; //sql statement
   $query = mysqli_query($con, $sql); //sql query
-  $result = mysqli_fetch_assoc($query); //sql result
+  //$result = mysqli_fetch_assoc($query); //sql result
 
-  //print_r($result);
-
-  //foreach(){
+  while( $row = mysqli_fetch_assoc($query)){
 ?>
   <tr>
-    <td>testing</td>
-    <td>testing</td>
+    <td><?php echo $row['title']; ?></td>
+    <td><?php echo $row['text']; ?></td>
   </tr>
-<?php// } ?>
+<?php } ?>
 </table>
-<br/><br/><br/><br/><br/><br/><br/>
+
 
 <?php
-          //0    //1   //2    //3     //4    //5
-$arr = ['moi', 'hai', 'ba', 'four', 'five', 'ennum'];
-echo json_encode($arr);
-//echo $arr[2];
-
-//echo sizeof($arr);
-
-echo "for loop";
-echo "<br/>";
-for( $i=0;$i<sizeof($arr);$i++ ){
-  echo $arr[$i];
-  echo "<br/>";
-}
-
-echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
-//echo "foreach loop";
-$arr = (object) $arr;
-echo json_encode($arr);
-foreach( $arr as $X ){
-  echo "<h2>".$X."</h2>";
-}
+//           //0    //1   //2    //3     //4    //5
+// $arr = ['moi', 'hai', 'ba', 'four', 'five', 'ennum'];
+// echo json_encode($arr);
+// //echo $arr[2];
+//
+// //echo sizeof($arr);
+//
+// echo "for loop";
+// echo "<br/>";
+// for( $i=0;$i<sizeof($arr);$i++ ){
+//   echo $arr[$i];
+//   echo "<br/>";
+// }
+//
+// echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+// //echo "foreach loop";
+// $arr = (object) $arr;
+// echo json_encode($arr);
+// foreach( $arr as $X ){
+//   echo "<h2>".$X."</h2>";
+// }
 
 
  ?>
